8c9b9c1ccfe12793860b299770634f5cce133139
Jack Teasell <contact.jackteasell@gmail.com>
2018-09-07 17:56:06 -0700

    hello

---
+++ app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+function bio() {
+  var txt = document.getElementById('text');
+  txt.innerHTML = 'This will be my bio';
+}
+
+function portfolio() {
+  var txt = document.getElementById('text');
+  txt.innerHTML = 'This will be my portfolio';
+}
+
+function contact() {
+  var txt = document.getElementById('text');
+  txt.innerHTML = 'This will be my Contact info';
+}
