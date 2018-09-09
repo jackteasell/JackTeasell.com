52b0fb6ff31c64a0419bc02d760c46de97331b8e
Jack Teasell <contact.jackteasell@gmail.com>
2018-09-09 15:24:44 -0700

    Fixed for mobile

--- app.js
+++ app.js
@@ -2,15 +2,15 @@
 
 function bio() {
   var txt = document.getElementById('text');
-  txt.innerHTML = 'This will be my bio';
+  txt.innerHTML = 'I am 20 years old and a software developer, I have been very fortunate to have spent time travelling all around the world, not going to a traditional highschool gave me a much more different experience than most people. Right now I just got out of my first year at Montana State University studying computer science. Going to University wasn\'t what I was used to, taking a traditional path of education was the exact opposite of what my life growing up was like. So I decided to take this path to continue to grow as a software developer. My passion is creating projects and products that can make else\'s someone\'s life easier.';
 }
 
 function portfolio() {
   var txt = document.getElementById('text');
-  txt.innerHTML = 'This will be my portfolio';
+  txt.innerHTML = 'coming soon!';
 }
 
 function contact() {
   var txt = document.getElementById('text');
-  txt.innerHTML = 'This will be my Contact info';
+  txt.innerHTML = 'Phone: 4254955202 <br> Email: contact.jackteasell@gmail.com <br> Social Media:     <ul><li><a href="https://github.com/jackteasell">  GitHub</a></li><li><a href="https://www.instagram.com/jackteasell/">  Instagram</a></li><li><a href="https://twitter.com/JackTeasell">  Twitter</a></li></ul>';
 }
